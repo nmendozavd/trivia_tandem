075d5861bf86c4d26fa6cb5eb6b6043d0dde8060
Noel Mendoza <nmendozavd@gmail.com>
2020-11-01 14:07:36 -0800

    added rendering component test

--- src/components/Questions.test.js
+++ src/components/Questions.test.js
@@ -3,8 +3,16 @@ import { render } from "@testing-library/react";
 import Questions from "./Questions.jsx";
 
 test("renders without crashing", () => {
+  let currentQuestion = 0;
+  let triviaData = [ 
+    {
+      "question": "What was Tandem previous name?",
+      "answers": ["Tandem", "Burger Shack", "Extraordinary Humans", "Devmynd"],
+      "correct": "Devmynd"
+    }
+  ]
   const div = document.createElement("div");
-  render(<Questions />, div);
+  render(<Questions currentQuestion = { currentQuestion } triviaData = { triviaData }/>, div);
 });
 
 
