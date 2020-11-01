887e983cfd7ed41b12f40ac05e39446ce6024d2d
Noel Mendoza <nmendozavd@gmail.com>
2020-11-01 13:15:14 -0800

    added alt to image

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Questions = ({ currentQuestion, triviaData, count }) => (
   <div className='question-section'>
-    <img src="tandem.png" />
+    <img src="tandem.png" alt="tandem logo"/>
     <div className='count-section'> Trivia Question: {count} of 10 </div>
     <div> {triviaData[currentQuestion].question} </div>
   </div>
